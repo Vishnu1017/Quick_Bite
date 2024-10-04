bcec8b32e745b5c33f8cf7e6a006225209ab9a9d
Vishnu1017 <playroll.vish@gmail.com>
2024-10-04 20:04:40 +0530

    Changed many thing in the app

--- lib/pages/onboard.dart
+++ lib/pages/onboard.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quick_bite/pages/login.dart';
 import 'package:quick_bite/widget/content_model.dart';
-import 'package:quick_bite/widget/widget_support.dart';
 
 class Onboard extends StatefulWidget {
   const Onboard({super.key});
@@ -10,19 +9,25 @@ class Onboard extends StatefulWidget {
   State<Onboard> createState() => _OnboardState();
 }
 
-class _OnboardState extends State<Onboard> {
+class _OnboardState extends State<Onboard> with SingleTickerProviderStateMixin {
   int currentIndex = 0;
   late PageController _controller;
+  late AnimationController _animationController;
 
   @override
   void initState() {
-    _controller = PageController(initialPage: 0);
     super.initState();
+    _controller = PageController(initialPage: 0);
+    _animationController = AnimationController(
+      duration: const Duration(milliseconds: 600),
+      vsync: this,
+    );
   }
 
   @override
   void dispose() {
     _controller.dispose();
+    _animationController.dispose();
     super.dispose();
   }
 
@@ -30,77 +35,136 @@ class _OnboardState extends State<Onboard> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        margin: EdgeInsets.only(top: 70),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.blue[700]!, Colors.blue[400]!],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
         child: Column(
           children: [
             Expanded(
               child: PageView.builder(
-                  controller: _controller,
-                  itemCount: contents.length,
-                  onPageChanged: (int index) {
-                    setState(() {
-                      currentIndex = index;
-                    });
-                  },
-                  itemBuilder: (_, i) {
-                    return Padding(
-                      padding: const EdgeInsets.only(left: 20, right: 20),
-                      child: Column(
-                        children: [
-                          Image.asset(
-                            contents[i].image,
-                            height: 450,
-                            width: MediaQuery.of(context).size.width / 1.2,
-                            fit: BoxFit.cover,
+                controller: _controller,
+                itemCount: contents.length,
+                onPageChanged: (int index) {
+                  setState(() {
+                    currentIndex = index;
+                  });
+                },
+                itemBuilder: (_, i) {
+                  return Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      ClipPath(
+                        clipper: CustomWaveClipper(),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.blue.withOpacity(0.3),
+                                blurRadius: 20,
+                                offset: const Offset(0, 10),
+                              ),
+                            ],
                           ),
-                          const SizedBox(height: 40),
-                          Text(contents[i].title,
-                              style: AppWidget.HeadLineTextFeildStyle()),
+                        ),
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Hero(
+                            tag: contents[i].image,
+                            child: Image.asset(
+                              contents[i].image,
+                              height: MediaQuery.of(context).size.height * 0.5,
+                              width: MediaQuery.of(context).size.width * 0.85,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
                           const SizedBox(height: 20),
                           Text(
-                            contents[i].description,
-                            style: AppWidget.LightTextFeildStyle(),
-                          )
+                            contents[i].title,
+                            style: TextStyle(
+                              fontSize: 26,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.blue[900],
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 30),
+                            child: Text(
+                              contents[i].description,
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.blue[600],
+                              ),
+                            ),
+                          ),
                         ],
                       ),
-                    );
-                  }),
+                    ],
+                  );
+                },
+              ),
             ),
-            Container(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: List.generate(
-                    contents.length, (index) => buildDot(index, context)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: List.generate(
+                contents.length,
+                (index) => buildDot(index, context),
               ),
             ),
             GestureDetector(
               onTap: () {
                 if (currentIndex == contents.length - 1) {
-                  Navigator.pushReplacement(context,
-                      MaterialPageRoute(builder: (context) => const LogIn()));
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => const LogIn()),
+                  );
+                } else {
+                  _controller.nextPage(
+                    duration: const Duration(milliseconds: 400),
+                    curve: Curves.easeInOut,
+                  );
                 }
-                _controller.nextPage(
-                  duration: const Duration(microseconds: 100),
-                  curve: Curves.bounceIn,
-                );
               },
               child: Container(
-                decoration: BoxDecoration(
-                    color: Colors.red, borderRadius: BorderRadius.circular(15)),
-                height: 50,
+                height: 60,
                 margin: const EdgeInsets.all(40),
                 width: double.infinity,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [Colors.blue[800]!, Colors.blue[900]!],
+                  ),
+                  borderRadius: BorderRadius.circular(30),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black38,
+                      blurRadius: 8,
+                      offset: const Offset(0, 4),
+                    ),
+                  ],
+                ),
                 child: Center(
                   child: Text(
-                    currentIndex == contents.length - 1 ? "Start" : "Next",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20),
+                    currentIndex == contents.length - 1
+                        ? "Get Started"
+                        : "Next",
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
                   ),
                 ),
               ),
-            )
+            ),
           ],
         ),
       ),
@@ -113,7 +177,36 @@ class _OnboardState extends State<Onboard> {
       width: currentIndex == index ? 18 : 7,
       margin: const EdgeInsets.only(right: 5),
       decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(6), color: Colors.black38),
+        borderRadius: BorderRadius.circular(6),
+        color: currentIndex == index ? Colors.blue[900] : Colors.black38,
+      ),
     );
   }
+}
+
+// Custom Clipper for Wave Effect
+class CustomWaveClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    path.lineTo(0, size.height - 80);
+    path.quadraticBezierTo(
+      size.width / 4,
+      size.height,
+      size.width / 2,
+      size.height - 40,
+    );
+    path.quadraticBezierTo(
+      size.width * 3 / 4,
+      size.height - 80,
+      size.width,
+      size.height - 40,
+    );
+    path.lineTo(size.width, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => true;
 }
